416d6ce328c4a3bf386c4f2b308bd57d49bd7e50
danylaksono <thalibul.ilm@gmail.com>
2018-08-04 22:45:21 +0700

    yogya

--- src/main.js
+++ src/main.js
@@ -80,7 +80,7 @@ const map = new maptalks.Map('map-main', {
     // center: [-0.113049, 51.498568],
     // center: [-73.97332, 40.76462],
     //center: [-74.0130345, 40.70635160000003],
-    center: [-7.7722716, 110.3754073],
+    center: [110.3754073, -7.7722716],
     zoom: 16,
     baseLayer: mainLayer
 });
